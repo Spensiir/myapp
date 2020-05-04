447039933f133ced363366953f108ec5a8a49107
Spencer Huang <sirsh8@gmail.com>
2020-05-04 11:26:05 -0400

    save leads collected on landing page
    we save email addresses collected back to database

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,4 @@
+const models = require('../models');
 
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
@@ -5,6 +6,10 @@ exports.get_landing = function(req, res, next) {
 
 
 exports.submit_lead = function(req, res, next) {
-  console.log("lead_email = ", req.body.lead_email);
-  res.redirect('/');
+
+  return models.Lead.create({
+  	email: req.body.lead_email
+  }).then(lead => {
+  	res.redirect('/')
+  })
 }
