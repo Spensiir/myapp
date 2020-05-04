804f6d83b41b5d28f7f9d90178ed4b3921bc41b5
Spencer Huang <sirsh8@gmail.com>
2020-05-04 12:11:54 -0400

    Implement /leads route
    show list of leads upon submission

--- controllers/landing.js
+++ controllers/landing.js
@@ -10,6 +10,15 @@ exports.submit_lead = function(req, res, next) {
   return models.Lead.create({
   	email: req.body.lead_email
   }).then(lead => {
-  	res.redirect('/')
+  	res.redirect('/leads');
   })
+}
+
+exports.show_leads = function(req, res, next) {
+	//asynchronous method defined with sequelize
+	return models.Lead.findAll().then(leads => {
+		console.log(leads.email);
+		res.render('landing', { title: 'Express', leads: leads });
+	})
+  
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ let landing = require('../controllers/landing.js');
 /* GET home page. */
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads)
 
 module.exports = router;
